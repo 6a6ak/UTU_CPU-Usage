02cd3872d280b3d0b43117ee7d07701e848995c9
6a6ak <babak.ahari@gmail.com>
2023-12-11 18:15:17 +0100

    repalce printf

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -3,8 +3,8 @@
 bg_color='\033[44m' # blue background
 text_color='\033[97m' # white text
 while true; do
-    # Get the CPU usage as a percentage
-    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    # Get the CPU usage as a percentage and format it as a two-digit number
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.0f\n", $2 + $4}')
     # Define an array with the characters to be displayed
     chars=( "⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏" )
     # Loop through the array and display each character for one second
